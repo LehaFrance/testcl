8cd7870792f78841c5a8de94f06e3724376f7d3a
Sylvain <smaignan@leha-labo.com>
2013-03-21 16:54:11 +0100

    test pour phpunit

--- src/Leha/UserBundle/Controller/BobController.php
+++ src/Leha/UserBundle/Controller/BobController.php
@@ -4,7 +4,7 @@ namespace Leha\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class BocController extends Controller
 {
     public function indexAction($name)
     {
